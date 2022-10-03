91a82f54eb078826efee6ca393ba124fb684da63
ektmd7676 <90033731+ektmd7676@users.noreply.github.com>
2022-10-03 09:36:02 +0700

    dd

---
+++ src/containers/Slide/style.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  .my-page{
+    width: 140px;
+  height: 38px;
+  margin: 0 88px 10px 261px;
+  font-family: NotoSansKR;
+  font-size: 26px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.46;
+  letter-spacing: normal;
+  text-align: left;
+  color: #3f444d;
+
+  }
+ .container{
+  margin-left: 261px;
+ }
+  .component{
+    margin-top: 10px;
+  }
+  
+    .dl {
+        width: 208px;
+  height: 45px;
+  margin: 10px 0 5px;
+  padding: 13px 20.2px 12px 25px;
+  border-radius: 5px;
+  background-color: #f5f5f5;
+  color: #5b5b5b;
+  display: flex;
+    flex-direction: column;
+    
+    }
+  
+  .container {
+    display: flex;
+    flex-direction: row;
+  }
+
+
+  `
--- src/pages/Mypage/index.tsx
+++ src/pages/Mypage/index.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Header from "containers/Header";
+import Body from './body/body'
+import Slide from '../../containers/Slide/index'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+ 
+} from "react-router-dom";
+import * as S from "./style";
+
 
 function MyPage() {
-  return <div>MyPage</div>;
+  return (
+  <div>
+    <S.Container>
+    <Header />
+      <Switch>  
+        <Router>
+        <Route exact path='/' component ={ Slide } />
+      
+        </Router>
+      </Switch>
+      </S.Container>
+   
+ 
+  </div>
+  )
 }
 
 export default MyPage;
